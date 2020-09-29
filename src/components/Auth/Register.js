f1e54593973b81111d1f2df2677f0e78822dc9ea
ErnestoArgentina <ernestoargentina@gmail.com>
2020-09-29 17:48:09 +0200

    Create Register.js

---
+++ src/components/Auth/Register.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import {Grid, Form, Segment, Button, Header, Message, Icon} from 'semantic-ui-react';
+import {Link} from 'react-router-dom'
+
+class Register extends React.Component{
+    state={
+        username: "",
+        email: "",
+        password: "",
+        passwordConfirmation: ""
+    }
+
+    handleChange= event =>{
+        this.setState({ [event.target.name]: event.target.value })
+    }
+
+    handleSubmit= event =>{
+        event.preventDefault
+    }
+
+    render(){
+        const { username, email, password, passwordConfirmation } = this.state
+        return(
+            <Grid textAlign="center" verticalAlign="middle" className="app">
+
+                <Grid.Column style={{maxWidth:450}}>
+                    <Header as="H3" color="orange" icon textAlign="center">
+                        <Icon name="puzzle piece" color="orange"></Icon>
+                        Register
+                    </Header>
+                    <Form onSubmit={this.handleSubmit} size="large">
+                        <Segment stacked>
+                            <Form.Input
+                            value={username}
+                             fluid
+                             name="username" 
+                             icon="user" 
+                             iconPosition="left" 
+                             placeholder="Username"
+                             onChange={this.handleChange}/>
+                      
+                            <Form.Input
+                            value={email}
+                             fluid
+                             name="email" 
+                             icon="mail" 
+                             iconPosition="left" 
+                             placeholder="Email"
+                             type="email"
+                             onChange={this.handleChange}/>
+                       
+                            <Form.Input
+                            value={password}
+                             fluid
+                             name="password" 
+                             icon="lock" 
+                             iconPosition="left" 
+                             placeholder="Password"
+                             type="password"
+                             onChange={this.handleChange}/>
+                        
+                            <Form.Input
+                            value={passwordConfirmation}
+                             fluid
+                             name="passwordConfirmation" 
+                             icon="lock" 
+                             iconPosition="left" 
+                             placeholder="Password Confirmation"
+                             type="password"
+                             onChange={this.handleChange}/>
+
+                             <Button color="orange" fluid size="large">Submit</Button>
+                        </Segment>
+                    </Form>
+                    <Message>Already a user? <Link to="/login">Login</Link> </Message>
+                    
+                </Grid.Column>
+                
+            </Grid>
+        )
+    }
+}
+
+export default Register;
